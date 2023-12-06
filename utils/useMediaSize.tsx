c487e920e797840713cf9f9c46cf740186d93d78
Luis Ballote <=>
2023-12-06 03:39:32 -0600

    changed useMediaQuery for other hook

--- utils/useMediaSize.tsx
+++ utils/useMediaSize.tsx
@@ -1,15 +1,32 @@
-import { useMediaQuery } from "@uidotdev/usehooks";
+"use client";
+import { useMediaQuery } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 export function useMediaSize() {
-  const isSmallDevice = useMediaQuery("only screen and (max-width : 639px)");
+  const isSmallDevice = useMediaQuery("only screen and (max-width : 639px)", {
+    ssr: true,
+    fallback: false,
+  });
   const isMediumDevice = useMediaQuery(
-    "only screen and (min-width : 640px) and (max-width : 939px)"
+    "only screen and (min-width : 640px) and (max-width : 939px)",
+    {
+      ssr: true,
+      fallback: false,
+    }
   );
   const isLargeDevice = useMediaQuery(
-    "only screen and (min-width : 940px) and (max-width : 1540px)"
+    "only screen and (min-width : 940px) and (max-width : 1540px)",
+    {
+      ssr: true,
+      fallback: false,
+    }
   );
   const isExtraLargeDevice = useMediaQuery(
-    "only screen and (min-width : 1541px)"
+    "only screen and (min-width : 1541px)",
+    {
+      ssr: true,
+      fallback: false,
+    }
   );
 
   return {
